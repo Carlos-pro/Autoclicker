8228def9e5a9aaafcf7332cc9fe646ffc2b02d17
Carlos-pro <carlos.prieto1986@gmail.com>
2022-04-07 10:08:43 +0200

    Chore: first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './main.css';
+
+import { Router } from '@vaadin/router';
+
+const outlet = document.getElementById('outlet');
+const router = new Router(outlet);
+
+router.setRoutes([
+    
+
+]);
